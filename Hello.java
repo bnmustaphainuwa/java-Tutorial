c7df366641e2586febf065467c47e8f3382e0b95
Ibrahim Mustapha Inuwa <bnmustaphainuwa@gmail.com>
2024-07-23 15:10:04 +0100

    First lesson

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello {
+    //main metho
+    public static void main(String[] args) {
+        //create a scanner
+        Scanner input =  new Scanner(System.in);
+        //prompt the user to enter his name
+        System.out.print("Enter your name:");
+        String name = input.nextLine();
+        //print the name to the user
+        System.out.println("Your name is: " + name);
+    }
+}
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    private String first_name;
+    private String last_name;
+    private long id;
+
+    //create a default constructor of the class student
+    Student(){}
+}
